b8fdccf932e0a89fa9a10548120d2dbadeba1e65
KevinBessette <111305425+KevinBessette@users.noreply.github.com>
2023-10-17 15:45:39 -0400

    juste keep readme

--- src/components/helloworld.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-
-export default function helloworld() {
-    const element = $('<div>Helloworld from Webpack</div>');
-    return element;
-}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import $ from 'jquery';
-import helloworld from './components/helloworld.js'
-
-$('body').append(helloworld());
